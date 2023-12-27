962515785f54c0bd88d608885063ba537be227b1
ermalsalihaj <ermalsalihaj03@gmail.com>
2023-12-28 00:55:56 +0100

    Home Page & Slider

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,14 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./main.css";
 import SideMenu from "../components/SideMenu";
 import Header from "./Header";
+import Home from "./Home";
 
 function Main() {
   const [active, setActive] = useState(false);
+  const [games, setGames] = useState([]);
 
   const handleToggleActive = () => {
     setActive(!active);
   };
+
+  const fetchData = () => {
+    fetch("http://localhost:3000/api/gamesData.json")
+      .then((res) => res.json())
+      .then((data) => {
+        setGames(data);
+      })
+      .catch((e) => console.log(e.message));
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
 
   return (
     <main>
@@ -16,6 +31,9 @@ function Main() {
       <div className={`banner ${active ? "active" : undefined}`}>
         <Header toggleActive={handleToggleActive} />
         {/* <Header {...{ toggleActive: handleToggleActive }} /> */}
+        <div className="container-fluid">
+          <Home games={games} />
+        </div>
       </div>
     </main>
   );
